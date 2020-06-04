1bca5c7891b6e3870430218f77900c3d6bc68548
Line'R <serinity320@gmail.com>
2020-06-04 23:26:07 +0300

    Revert "update 7"
    This reverts commit 3eae4d839c75af94a1e64c3fec3c8b9518e17d23.

--- app/src/main/java/com/liner/i_desk/Utils/Views/FirebaseActivity.java
+++ app/src/main/java/com/liner/i_desk/Utils/Views/FirebaseActivity.java
@@ -2,7 +2,6 @@ package com.liner.i_desk.Utils.Views;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.inputmethod.InputMethodManager;
 
@@ -17,13 +16,15 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.liner.i_desk.API.Data.Request;
 import com.liner.i_desk.API.Data.User;
 import com.liner.i_desk.API.FirebaseHelper;
 import com.liner.i_desk.Utils.BroadcastManager;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public abstract class FirebaseActivity extends FragmentActivity {
@@ -35,14 +36,12 @@ public abstract class FirebaseActivity extends FragmentActivity {
     public FirebaseUser firebaseUser;
     public User user;
     public BroadcastManager broadcastManager;
-    public String FIREBASE_ACTION_USER_ADDED = "com.liner.i_desk.FIREBASE_ACTION_USER_ADDED";
-    public String FIREBASE_ACTION_DATABSE_CHANGED = "com.liner.i_desk.FIREBASE_ACTION_DATABSE_CHANGED";
-    public String FIREBASE_ACTION_USER_CHANGED = "com.liner.i_desk.FIREBASE_ACTION_USER_CHANGED";
-    public String FIREBASE_ACTION_USER_DELETED = "com.liner.i_desk.FIREBASE_ACTION_USER_DELETED";
+    public String FIREBASE_ACTION = "com.liner.i_desk.FIREBASE_CHANGED";
+    public String FIREBASE_ACTION_USER = "com.liner.i_desk.FIREBASE_ACTION_USER";
+
 
 
     public boolean hideKeyboard = true;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         firebaseActivity = this;
@@ -52,157 +51,108 @@ public abstract class FirebaseActivity extends FragmentActivity {
         firebaseDatabase = FirebaseDatabase.getInstance();
         storageReference = FirebaseStorage.getInstance().getReference();
         usersDatabase = firebaseDatabase.getReference().child("Users");
-        new FirebaseExtender<User>(usersDatabase) {
+        usersDatabase.keepSynced(true);
+        FirebaseHelper.getUserModel(new FirebaseHelper.IFirebaseHelperListener() {
             @Override
-            public void onItemAdded(String key, User item, int pos) {
-                if(item.getUserUID().equals(firebaseUser.getUid())){
-                    user = item;
-                    onUserReceived(key, item, pos);
-                    broadcastManager.sendLocal(FIREBASE_ACTION_USER_ADDED);
-                } else {
-                    onReceived(key, item, pos);
+            public void onSuccess(Object result) {
+                user = (User) result;
+                FirebaseHelper.getRequests(user, new FirebaseHelper.IFirebaseHelperListener() {
+                    @Override
+                    public void onSuccess(Object result) {
+                        broadcastManager.sendLocal(FIREBASE_ACTION_USER);
+                        onUserObtained(user);
+                    }
+
+                    @Override
+                    public void onFail(String reason) {
+                        broadcastManager.sendLocal(FIREBASE_ACTION_USER);
+                        onUserObtained(user);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onFail(String reason) {
+                user = null;
+            }
+        });
+        usersDatabase.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                if(dataSnapshot.getKey().equals(firebaseUser.getUid())){
+                    user = dataSnapshot.getValue(User.class);
                 }
+                onFirebaseChanged();
+                broadcastManager.sendLocal(FIREBASE_ACTION);
+                updateUserList();
             }
 
             @Override
-            public void onItemChanged(String key, User oldItem, User newItem, int pos) {
-                if(newItem.getUserUID().equals(firebaseUser.getUid())){
-                    user = newItem;
-                    onUserChanged(key, oldItem, newItem, pos);
-                    broadcastManager.sendLocal(FIREBASE_ACTION_USER_CHANGED);
-                } else {
-                    onChanged(key, oldItem, newItem, pos);
+            public void onChildChanged(@NonNull final DataSnapshot dataSnapshot, @Nullable String s) {
+                if(dataSnapshot.getKey().equals(firebaseUser.getUid())){
+                    user = dataSnapshot.getValue(User.class);
                 }
+                onFirebaseChanged();
+                broadcastManager.sendLocal(FIREBASE_ACTION);
+                updateUserList();
             }
 
             @Override
-            public void onItemRemoved(String key, User item, int pos) {
-                if(item.getUserUID().equals(firebaseUser.getUid())){
-                    user = item;
-                    onUserRemoved(key, item, pos);
-                    broadcastManager.sendLocal(FIREBASE_ACTION_USER_DELETED);
-                } else {
-                    onRemoved(key, item, pos);
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getKey().equals(firebaseUser.getUid())){
+                    user = dataSnapshot.getValue(User.class);
                 }
+                onFirebaseChanged();
+                broadcastManager.sendLocal(FIREBASE_ACTION);
+                updateUserList();
             }
 
             @Override
-            public void onItemMoved(String key, User item, int pos, int newPos) {
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                if(dataSnapshot.getKey().equals(firebaseUser.getUid())){
+                    user = dataSnapshot.getValue(User.class);
+                }
+                onFirebaseChanged();
+                broadcastManager.sendLocal(FIREBASE_ACTION);
+                updateUserList();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
-        };
-
-
-//        usersDatabase.keepSynced(true);
-//        FirebaseHelper.getUserModel(new FirebaseHelper.IFirebaseHelperListener() {
-//            @Override
-//            public void onSuccess(Object result) {
-//                user = (User) result;
-//                FirebaseHelper.getRequests(user, new FirebaseHelper.IFirebaseHelperListener() {
-//                    @Override
-//                    public void onSuccess(Object result) {
-//                        broadcastManager.sendLocal(FIREBASE_ACTION_USER);
-//                        onUserObtained(user);
-//                    }
-//
-//                    @Override
-//                    public void onFail(String reason) {
-//                        broadcastManager.sendLocal(FIREBASE_ACTION_USER);
-//                        onUserObtained(user);
-//                    }
-//                });
-//
-//            }
-//
-//            @Override
-//            public void onFail(String reason) {
-//                user = null;
-//            }
-//        });
-//        usersDatabase.addChildEventListener(new ChildEventListener() {
-//            @Override
-//            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//                if (dataSnapshot.getKey().equals(firebaseUser.getUid())) {
-//                    user = dataSnapshot.getValue(User.class);
-//                }
-//                onFirebaseChanged();
-//                broadcastManager.sendLocal(FIREBASE_ACTION);
-//                updateUserList();
-//            }
-//
-//            @Override
-//            public void onChildChanged(@NonNull final DataSnapshot dataSnapshot, @Nullable String s) {
-//                if (dataSnapshot.getKey().equals(firebaseUser.getUid())) {
-//                    user = dataSnapshot.getValue(User.class);
-//                }
-//                onFirebaseChanged();
-//                broadcastManager.sendLocal(FIREBASE_ACTION);
-//                updateUserList();
-//            }
-//
-//            @Override
-//            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-//                if (dataSnapshot.getKey().equals(firebaseUser.getUid())) {
-//                    user = dataSnapshot.getValue(User.class);
-//                }
-//                onFirebaseChanged();
-//                broadcastManager.sendLocal(FIREBASE_ACTION);
-//                updateUserList();
-//            }
-//
-//            @Override
-//            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-//                if (dataSnapshot.getKey().equals(firebaseUser.getUid())) {
-//                    user = dataSnapshot.getValue(User.class);
-//                }
-//                onFirebaseChanged();
-//                broadcastManager.sendLocal(FIREBASE_ACTION);
-//                updateUserList();
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
+        });
         super.onCreate(savedInstanceState);
     }
 
+    public abstract void onFirebaseChanged();
+    public abstract void onUserObtained(User user);
 
-    public abstract void onUserReceived(String key, User user, int pos);
-    public abstract void onUserChanged(String key, User oldValue, User newValue, int pos);
-    public abstract void onUserRemoved(String key, User user, int pos);
-    public abstract void onReceived(String key, User user, int pos);
-    public abstract void onChanged(String key, User oldValue, User newValue, int pos);
-    public abstract void onRemoved(String key, User user, int pos);
+    public void updateUserList(){
+        FirebaseHelper.getRequests(user, new FirebaseHelper.IFirebaseHelperListener() {
+            @Override
+            public void onSuccess(Object result) {
+                broadcastManager.sendLocal(FIREBASE_ACTION_USER);
+                onUserObtained(user);
+            }
 
-
-
-
-//    public void updateUserList() {
-//        FirebaseHelper.getRequests(user, new FirebaseHelper.IFirebaseHelperListener() {
-//            @Override
-//            public void onSuccess(Object result) {
-//                broadcastManager.sendLocal(FIREBASE_ACTION_USER);
-//                onUserObtained(user);
-//            }
-//
-//            @Override
-//            public void onFail(String reason) {
-//                broadcastManager.sendLocal(FIREBASE_ACTION_USER);
-//                onUserObtained(user);
-//            }
-//        });
-//    }
+            @Override
+            public void onFail(String reason) {
+                broadcastManager.sendLocal(FIREBASE_ACTION_USER);
+                onUserObtained(user);
+            }
+        });
+    }
 
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-        if (hideKeyboard)
-            if (getCurrentFocus() != null) {
-                InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                Objects.requireNonNull(inputMethodManager).hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
-            }
+        if(hideKeyboard)
+        if (getCurrentFocus() != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            Objects.requireNonNull(inputMethodManager).hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
         return super.dispatchTouchEvent(ev);
     }
 }
